c112c0e4e6443685ce869e894729b9f852a08db4
Marc <marcubb@outlook.de>
2022-11-23 00:19:36 +0100

    added WPS example and edit readme

--- README.md
+++ README.md
@@ -1,10 +1,49 @@
 # IR-Controller
 This repository contains the software for an infrared controller, the purpose of which is to be able to control devices with an infrared interface using a smartphone.
 
-#### JSON-Format:
+### JSON-Format:
 ```
+data.json:
 {
-  "name":"Philips TV", 
-  "sequence": [21,24,71,34,74,73,21,60,32]
+  {
+    "name":"Philips TV", 
+    "sequence": [21,24,71,34,74,73,21,60,32]
+  }
 }
 ```
+### Setup:
+#### WifiManager
+- always starts on startup to either connect to wifi or configure new wifi
+- saves Network credentials at same location as WPS (examples already do that automatically)
+
+#### WPS Interrupt
+- alternatively to WifiManager its possible to connect via WPS
+- an Interrupt is triggered on pressing the WPS Button
+- saves Network credentials at same location as WifiManager (examples already do that automatically)
+
+### Methodes:
+#### read_signal(str name)
+1. turns Signal LED on
+2. reads incoming IR-Signal with defined minimum length
+
+  2.1 IR-Signal is received -> 3.
+  
+  2.2 No IR-Signal is received -> Signal LED Blinks and Timeout is sent
+  
+3. turns Signal LED off
+4. creates new entry in data.json and writes name and sequence to file
+
+#### play_signal(str name)
+1. looks for entry with given name
+
+  1.1 Entry is found -> 2.
+  
+  1.2 Entry is not found -> try to delete Entry from frontend
+  
+2. plays entered sequence
+
+#### 
+
+
+### Annotations:
+- #include <WiFiManager.h> in main.cpp otherwise error in library occures
---
+++ examples/WPS_test.cpp
@@ -0,0 +1,58 @@
+#include <ESP8266WiFi.h>
+#define WPS D4 //Pin für den WPS Taster
+
+
+//Startet die WPS Konfiguration
+bool startWPS() {
+  Serial.println("WPS Konfiguration gestartet");
+  bool wpsSuccess = WiFi.beginWPSConfig();
+  if(wpsSuccess) {
+      // Muss nicht immer erfolgreich heißen! Nach einem Timeout bist die SSID leer
+      String newSSID = WiFi.SSID();
+      if(newSSID.length() > 0) {
+        // Nur wenn eine SSID gefunden wurde waren wir erfolgreich 
+        Serial.printf("WPS fertig. Erfolgreich angemeldet an SSID '%s'\n", newSSID.c_str());
+      } else {
+        wpsSuccess = false;
+      }
+  }
+  return wpsSuccess; 
+}
+
+//Setup Funktion
+void setup() {
+  Serial.begin(74880); //mit 74880 sind auch die Meldungen beim Start sichtbar
+  Serial.setDebugOutput(true); //Wenn true werden zusätzliche Debug Informationen ausgegeben
+  delay(1000);
+  Serial.printf("\nVersuche Verbindung mit gespeicherter SSID '%s'\n", WiFi.SSID().c_str());
+  pinMode(WPS, INPUT_PULLUP); //Taster Eingang aktivieren
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WiFi.SSID().c_str(),WiFi.psk().c_str()); // letzte gespeicherte Zugangsdaten
+  int cnt = 0;
+  //Wir versuchen eine Anmeldung
+  while ((WiFi.status() == WL_DISCONNECTED) && (cnt < 10)){
+    delay(500);
+    Serial.print(".");
+    cnt++;
+  }
+
+  wl_status_t status = WiFi.status();
+  if(status == WL_CONNECTED) {
+    Serial.printf("\nErfolgreich angemeldet an SSID '%s'\n", WiFi.SSID().c_str());
+  } else {
+    //Wir waren nicht erfolgreich starten daher WPS
+    Serial.printf("\nKann keine WiFi Verbindung herstellen. Status ='%d'\n", status); 
+    Serial.println("WPS Taste am Router drücken.\n WPS Taste am ESP drücken!");
+    while (digitalRead(WPS)!=0) {yield();}   
+    if(!startWPS()) {
+       Serial.println("Keine Verbindung über WPS herstellbar");  
+    }
+  } 
+}
+
+
+void loop() {
+  // Code fürs Programm
+
+}
