a40b240b901d1772333a0b3ee8f5b3a951593c1f
Marc <marcubb@outlook.de>
2022-12-07 00:44:24 +0100

    finalized adapter code

--- include/functions.h
+++ include/functions.h
@@ -11,235 +11,19 @@
 #include <ArduinoJson.h>
 #include <LittleFS.h>
 
+String captureSignal();
+DynamicJsonDocument convertToJSON(String result_string, String name);
+void saveCommand(String filename, DynamicJsonDocument doc);
+DynamicJsonDocument loadCommand(String filename);
+void sendCommand(DynamicJsonDocument doc);
+void listFiles(String directory);
+boolean checkFile(String filename);
+void printFile(String filename);
+
 
 // TODO: Umschreiben, sodass alle Sequenzen in data/Sequenzname, also in eigener Datei gespeichert werden
+// TODO: call by reference woimmer es Sinn macht und geht
 
-
-// TODO: lädt JSON aus Datei und liest Eintrag
-/*
-void loadCommand(String filename, int sequence[], String filter) {
-  // Open file for reading
-  File file = SPIFFS.open(filename, "r");
-
-  // Allocate a temporary JsonDocument
-  // Don't forget to change the capacity to match your requirements.
-  // Use arduinojson.org/v6/assistant to compute the capacity.
-  StaticJsonDocument<3096> doc;
-
-  // Deserialize the JSON document
-  DeserializationError error = deserializeJson(doc, file);
-  if (error)
-    Serial.println(F("Failed to read file, using default configuration"));
-
-  // Copy values from the JsonDocument to the Config
-  config.port = doc["port"] | 2731;
-  strlcpy(config.hostname,                  // <- destination
-          doc["hostname"] | "example.com",  // <- source
-          sizeof(config.hostname));         // <- destination's capacity
-
-  // Close the file (Curiously, File's destructor doesn't close the file)
-  file.close();
-
-  // https://www.geeksforgeeks.org/convert-a-string-to-integer-array-in-c-c/
-  // get length of string str
-  int str_length = output.length();
-
-  // create an array with size as string
-  // length and initialize with 0
-  int arr[str_length] = { 0 };
-  int j = 0, i, sum = 0;
-  // Traverse the string
-  for (i = 0; output[i] != '&#092;&#048;'; i++) {
-
-    // if str[i] is ', ' then split
-    if (output[i] == ',')
-      continue;
-    if (output[i] == ' '){
-      // Increment j to point to next
-      // array location
-      j++;
-    }
-    else {
-
-    // subtract str[i] by 48 to convert it to int
-    // Generate number by multiplying 10 and adding
-    // (int)(str[i])
-    arr[j] = arr[j] * 10 + (output[i] - 48);
-    }
-  }
-  return arr;
-}
-*/
-
-
-// TODO: testen
-void sendCommand(String result_string) {
-  
-  // extracts length and sequence from String
-  unsigned first = result_string.indexOf("[");
-  unsigned last = result_string.indexOf("]");
-  int length = result_string.substring(first +1,last).toInt();
-
-  first = result_string.indexOf("{");
-  last = result_string.indexOf("}");
-  String sequence = result_string.substring (first +1,last);
-  
-  int kIrLed = 4;  // ESP8266 GPIO pin to use. Recommended: 4 (D2).
-
-  IRsend irsend(kIrLed);  // Set the GPIO to be used to sending the message.
-
-  // Example of data captured by IRrecvDumpV2.ino
-  uint16_t command[length] = {0};
-  int j = 0, i;
-
-  for (i = 0; sequence[i] != '&#092;&#048;'; i++) {
-
-    // https://www.geeksforgeeks.org/convert-a-string-to-integer-array-in-c-c/
-    // if str[i] is ', ' then split
-    if (sequence[i] == ',')
-      continue;
-    if (sequence[i] == ' '){
-      // Increment j to point to next
-      // array location
-      j++;
-    }
-    else {
-
-      // subtract str[i] by 48 to convert it to int
-      // Generate number by multiplying 10 and adding
-      // (int)(str[i])
-      command[j] = command[j] * 10 + (sequence[i] - 48);
-    }
-  }
-
-  irsend.begin();
-
-  Serial.println("a rawData capture from IRrecvDumpV2");
-  irsend.sendRaw(command, length, 38);  
-  
-}
-
-// TODO: lädt JSON aus Datei und entfernt Eintrag mit Komma
-void deleteCommand(String name) {
-  return;
-}
-
-
-DynamicJsonDocument convertToJSON(String result_string, String name){
-
-  // extracts length and sequence from String
-  unsigned first = result_string.indexOf("[");
-  unsigned last = result_string.indexOf("]");
-  String length = result_string.substring (first +1,last);
-
-  first = result_string.indexOf("{");
-  last = result_string.indexOf("}");
-  String sequence = result_string.substring (first +1,last);
-
-  DynamicJsonDocument doc(3096);
-  doc["name"] = name;
-  doc["length"] = length.toInt();
-  doc["sequence"] = sequence;
-  doc.shrinkToFit();
-  delay(1000);
-  Serial.println("In JSON format:");
-  serializeJson(doc, Serial);
-  return doc;
-}
-
-
-void deleteFile(String filename) {
-  LittleFS.begin();
-  LittleFS.remove(filename);
-  LittleFS.end();
-  return;
-}
-
-
-void createFile(String filename, boolean replace) {
-  LittleFS.begin();
-
-  // File exists already:
-  if (LittleFS.exists(filename)) {
-    if (replace == false) {
-      Serial.println("File exists already!");
-      LittleFS.end();
-      return;
-    }
-    else {
-      LittleFS.remove(filename);
-      File myfile = LittleFS.open(filename, "w");
-      myfile.close();
-      LittleFS.end();
-      return;
-    }
-  }
-
-  // File doesn't exist yet:
-  else{
-    File myfile = LittleFS.open(filename, "w");
-    myfile.close();
-    LittleFS.end();
-    return;
-  }
-}
-
-
-void initFile (String filename) {
-  LittleFS.begin();
-  File myfile = LittleFS.open(filename, "w");
-  myfile.print("{\"data\":[]}");
-  myfile.close();
-  LittleFS.end();
-}
-
-
-void saveCommand(String filename, DynamicJsonDocument doc) {
-
-  LittleFS.begin();
-
-  File myfile = LittleFS.open(filename, "r+"); // See https://github.com/lorol/LITTLEFS/issues/33
-  myfile.seek((myfile.size()-2), SeekSet);
-
-  if (!myfile) {
-    Serial.println(F("Failed to create file"));
-    return;
-  }
-
-  if (myfile.size() > 11) {
-    myfile.print(",");
-  }
-
-  // Serialize JSON to file
-  if (serializeJson(doc, myfile) == 0) {
-    Serial.println(F("Failed to write to file"));
-  }
-  myfile.print("]}");
-  // Close the file
-  myfile.close();
-  LittleFS.end();
-}
-
-void printFile(String filename) {
-  LittleFS.begin();
-  File file2 = LittleFS.open(filename, "r");
-
-  if (!file2) {
-    Serial.println("Failed to open file for reading");
-    return;
-  }
-
-  Serial.println("");
-  Serial.println("JSON in file:");
-  while (file2.available()) {
-    Serial.write(file2.read());
-  }
-
-  file2.close();
-  LittleFS.end();
-}
-
-// TODO: handle case that no Signal was received
 // TODO: 65535 (max int value) appears regulary (fix)
 // receivePin = 14; captureBufferSize = 1024; timeoutSequence = 90; minUnknownSize = 12; timeoutRecording = 10000;
 String captureSignal(){
@@ -268,10 +52,13 @@ String captureSignal(){
     timestamp = millis() - start_time;
     if (timestamp % 1000 == 0){
       Serial.println((int)timestamp/1000);
+      delay(1);
     }
     // Check if the IR code has been received.
     if (irrecv.decode(&results)){
       if (results.overflow == false){
+        Serial.println("captured raw sequence:");
+        Serial.println(resultToSourceCode(&results));
         digitalWrite(ledPin, LOW);
         delay(100);
         digitalWrite(ledPin, HIGH);
@@ -298,4 +85,204 @@ String captureSignal(){
 
   Serial.println("No signal received");
   return "No Signal";
+}
+
+
+DynamicJsonDocument convertToJSON(String result_string, String name){
+
+  // extracts length and sequence from String
+  unsigned first = result_string.indexOf("[");
+  unsigned last = result_string.indexOf("]");
+  String length = result_string.substring (first +1,last);
+
+  first = result_string.indexOf("{");
+  last = result_string.indexOf("}");
+  String sequence = result_string.substring (first +1,last);
+
+  DynamicJsonDocument doc(3096);
+  doc["name"] = name;
+  doc["length"] = length.toInt();
+  doc["sequence"] = sequence;
+  doc.shrinkToFit();
+  delay(1000);
+  Serial.println("Converting Sequence to JSON format:");
+  serializeJson(doc, Serial);
+  return doc;
+}
+
+
+void saveCommand(String filename, DynamicJsonDocument doc) {
+
+  LittleFS.begin();
+
+  if (LittleFS.exists(filename)) {
+    LittleFS.remove(filename);
+  }
+
+  File myfile = LittleFS.open(filename, "w");
+  
+
+  if (!myfile) {
+    Serial.println(F("Failed to create file"));
+    return;
+  }
+
+  // Serialize JSON to file
+  if (serializeJson(doc, myfile) == 0) {
+    Serial.println(F("Failed to write to file"));
+  }
+  // Close the file
+  myfile.close();
+  LittleFS.end();
+}
+
+
+DynamicJsonDocument loadCommand(String filename) {
+  LittleFS.begin();
+  // Open file for reading
+  File myfile = LittleFS.open(filename, "r");
+
+  // Allocate a temporary JsonDocument
+  // Don't forget to change the capacity to match your requirements.
+  // Use arduinojson.org/v6/assistant to compute the capacity.
+  DynamicJsonDocument doc(3096);
+
+  if (!myfile) {
+    Serial.println("Failed to read file!");
+    return doc;
+  }
+
+  // Deserialize the JSON document
+  DeserializationError error = deserializeJson(doc, myfile);
+  if (error) {
+    Serial.println("Failed to read file, using default configuration");
+    LittleFS.remove(filename);
+  }
+
+  doc.shrinkToFit();
+  // Close the file (Curiously, File's destructor doesn't close the file)
+  myfile.close();
+  LittleFS.end();
+  return doc;
+
+}
+
+// TODO: zu call by reference ändern
+void sendCommand(DynamicJsonDocument doc) {
+
+  int length = doc["length"];
+
+  String sequence = doc["sequence"];
+  
+  int kIrLed = 4;  // ESP8266 GPIO pin to use. Recommended: 4 (D2).
+
+  IRsend irsend(kIrLed);  // Set the GPIO to be used to sending the message.
+
+  // https://stackoverflow.com/questions/48409822/convert-a-string-to-an-integer-array
+  unsigned int data_num = 0;
+  uint16_t command[length];
+  // loop as long as a comma is found in the string
+  while(sequence.indexOf(",")!=-1){
+    // take the substring from the start to the first occurence of a comma, convert it to int and save it in the array
+    command[ data_num ] = sequence.substring(0,sequence.indexOf(",")).toInt();
+    data_num++; // increment our data counter
+    //cut the data string after the first occurence of a comma
+    sequence = sequence.substring(sequence.indexOf(",")+1);
+  }
+  // get the last value out of the string, which as no more commas in it
+  command[ data_num ] = sequence.toInt();
+
+  irsend.begin();
+
+  irsend.sendRaw(command, length, 38);
+}
+
+
+// https://arduino.stackexchange.com/questions/76186/how-can-i-list-only-files-that-begin-with-log
+void checkFiles(String directory) {
+  LittleFS.begin();
+  Dir dir = LittleFS.openDir(directory);
+
+  while(dir.next()){
+    Serial.println();
+    Serial.print(dir.fileName());
+    Serial.print(" - ");
+    String filepath = "";
+    if (directory != "/") {
+      filepath += directory + "/" + dir.fileName();
+    }
+    else {
+      filepath += "/" + dir.fileName();
+    }
+    if(checkFile(filepath) == true) {
+      Serial.print("true");
+    }
+    else{
+      Serial.print("false");
+    }
+  }
+  Serial.println();
+  LittleFS.end();
+}
+
+boolean checkFile(String filename) {
+  LittleFS.begin();
+
+  File myfile = LittleFS.open(filename, "r");
+
+  if (!myfile) {
+    return false;
+  }
+
+  DynamicJsonDocument doc(3096);
+  DeserializationError error = deserializeJson(doc, myfile);
+  doc.shrinkToFit();
+  
+  if (error) {
+    myfile.close();
+    LittleFS.end();
+    doc.garbageCollect();
+    return false;
+  }
+
+  if (doc["length"] < 12 || doc["length"] > 2048) {
+    myfile.close();
+    LittleFS.end();
+    doc.garbageCollect();
+    return false;
+  }
+
+  String sequence = doc["sequence"];
+  int length = sequence.length();
+  if (sequence.substring(length - 1) == "," || sequence.substring(0, 1) == ",") {
+    myfile.close();
+    LittleFS.end();
+    doc.garbageCollect();
+    return false;
+  }
+
+  myfile.close();
+  LittleFS.end();
+  doc.garbageCollect();
+  return true;
+
+}
+
+void printFile(String filename) {
+  LittleFS.begin();
+  File file2 = LittleFS.open(filename, "r");
+
+  if (!file2) {
+    Serial.println("Failed to open file for reading");
+    return;
+  }
+
+  Serial.println("");
+  Serial.println("JSON in file:");
+  while (file2.available()) {
+    Serial.write(file2.read());
+  }
+
+  file2.close();
+  LittleFS.end();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -9,22 +9,30 @@
 
 void setup() {
   Serial.begin(115200);
-  //deleteFile("/data.txt");
-  //createFile("/data.txt", false);
-  //initFile("/data.txt");
+  delay(1000);
+  listFiles("/data");
+  // User choses to create a new sequence
+  // 1. filename is generated:
+  String filename = "/data/Sequenz1.txt";
+  // 2. signal is received
   String result_string = captureSignal();
-  Serial.println("As raw Sequence:");
-  Serial.println(result_string);
+  // 3. if sinal was received successfully
   if (result_string != "No Signal"){
+    // 3.1 signal String is serialized to JSON
     DynamicJsonDocument testJSON = convertToJSON(result_string, "PLACEHOLDER");
-    saveCommand("/data.txt", testJSON);
+    // 3.2 JSON is written to file
+    saveCommand(filename, testJSON);
     delay(1000);
-    printFile("/data.txt");
+    listFiles("/data");
+    // 3.3 loads Command and saves it in JSON Object
+    DynamicJsonDocument doc = loadCommand(filename);
+    // 3.4 (send command)
+    sendCommand(doc);
   }
 }
 
 void loop() {
-
+  delay(1000);
 }
 /*
 #include <ESP8266WiFi.h>
